e4ef6d488e67c888b5b76695b2146e6b8de78009
Maciej Sosnowski <maciej.sosnowski@exablaze.com>
2017-07-31 12:18:53 +1000

    exasock: add exasock_disable_acceleration() helper function

--- libs/exasock/include/exasock/socket.h
+++ libs/exasock/include/exasock/socket.h
@@ -5,6 +5,8 @@
 #ifndef EXASOCK_API_SOCKET_H
 #define EXASOCK_API_SOCKET_H
 
+#include <sys/socket.h>
+
 /** \brief Exasock private socket option level
  *
  * To manipulate socket options at Exasock level (i.e. manipulate an Exasock
@@ -25,5 +27,29 @@
  * This option takes an int value. This is a Boolean option.
  */
 #define SO_EXA_NO_ACCEL     1
+
+/**
+ * \brief Disable acceleration on the socket
+ *
+ * This is a helper function for disabling acceleration on the socket. This
+ * function can be used instead of calling directly setsockopt() with
+ * SO_EXA_NO_ACCEL Exasock private socket option.
+ * Disabling of acceleration on a socket is not allowed if the socket has
+ * already been accelerated (either by binding it to an ExaNIC interface
+ * or joining a multicast group with an ExaNIC interface). In such a case
+ * this function fails with EPERM error.
+ *
+ * \param[in]   fd
+ *      Exasock socket to disable acceleration on
+ *
+ * \return 0 on success, or -1 if an error occurred
+ */
+static inline int exasock_disable_acceleration(int fd)
+{
+    int disable = 1;
+
+    return setsockopt(fd, SOL_EXASOCK, SO_EXA_NO_ACCEL, &disable,
+                      sizeof(disable));
+}
 
 #endif /* EXASOCK_API_SOCKET_H */
