c47d9d52495f2304b0aeb08b233a9b3c1c21614c
Matthew Chapman <matthew.chapman@exablaze.com>
2017-08-04 12:33:37 +1000

    Bump version to 2.0.0, update changelog.txt

--- exanic.spec
+++ exanic.spec
@@ -1,5 +1,5 @@
 Name:           exanic
-Version:        1.8.1-git
+Version:        2.0.0-git
 Release:        1%{?dist}
 
 Summary:        ExaNIC drivers and software
--- util/exanic-config.c
+++ util/exanic-config.c
@@ -1945,7 +1945,7 @@ int ptp_command(const char *progname, const char *device,
     }
 
 usage_error:
-    fprintf(stderr, "exanic-config version 1.8.1-git\n");
+    fprintf(stderr, "exanic-config version 2.0.0-git\n");
     fprintf(stderr, "Detailed PTP grandmaster configuration and status:\n");
     fprintf(stderr, "   %s <device> ptp status\n", progname);
     fprintf(stderr, "   %s <device> ptp { enable | disable }\n", progname);
@@ -2163,7 +2163,7 @@ int main(int argc, char *argv[])
     }
 
 usage_error:
-    fprintf(stderr, "exanic-config version 1.8.1-git\n");
+    fprintf(stderr, "exanic-config version 2.0.0-git\n");
     fprintf(stderr, "Detailed network interface configuration and status:\n");
     fprintf(stderr, "   %s <device> [-v]\n", argv[0]);
     fprintf(stderr, "   %s <device> sfp status\n", argv[0]);
