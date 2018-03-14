958297b5d1b2fb467d159d8d79f68145c95c1415
Peter von Konigsmark <peter@exablaze.com>
2018-03-14 12:59:38 +1100

    exanic-clock-sync: Handle PPS (termination) setting for HPT and V5P

--- util/clock-sync/exanic_pps.c
+++ util/clock-sync/exanic_pps.c
@@ -105,7 +105,8 @@ struct exanic_pps_sync_state *init_exanic_pps_sync(const char *name, int clkfd,
         pps_type = PPS_DIFFERENTIAL;
     }
     else if (((hw_id == EXANIC_HW_X10) || (hw_id == EXANIC_HW_X10_GM) ||
-                (hw_id == EXANIC_HW_X40 || (hw_id == EXANIC_HW_V5P) )) &&
+                (hw_id == EXANIC_HW_X40 || (hw_id == EXANIC_HW_V5P) ||
+                (hw_id == EXANIC_HW_X10_HPT))) &&
             pps_type == PPS_DIFFERENTIAL)
     {
         log_printf(LOG_WARNING, "%s: %s does not support differential PPS input",
@@ -155,8 +156,10 @@ struct exanic_pps_sync_state *init_exanic_pps_sync(const char *name, int clkfd,
         exanic_register_write(state->exanic, REG_HW_INDEX(REG_HW_SERIAL_PPS), reg);
     }
     else if ((hw_id == EXANIC_HW_X10) || (hw_id == EXANIC_HW_X10_GM) ||
-            (hw_id == EXANIC_HW_X40))  /* PPS Termination Settings */
+            (hw_id == EXANIC_HW_X40) || (hw_id == EXANIC_HW_X10_HPT) ||
+            (hw_id == EXANIC_HW_V5P))
     {
+        /* PPS Termination Settings */
         uint32_t reg;
 
         reg = exanic_register_read(state->exanic, REG_HW_INDEX(REG_HW_SERIAL_PPS));
