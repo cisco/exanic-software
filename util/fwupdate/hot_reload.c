131e7fd56c1841c7d75df5489d3c09607e306c29
Matthew Chapman <matthew.chapman@exablaze.com>
2018-09-04 17:31:33 +1000

    exanic-fwupdate: Mask off "surprise down" event on the parent PCIe bridge during hot reload, to prevent causing fatal PCIe bus errors on some servers.

--- util/fwupdate/hot_reload.c
+++ util/fwupdate/hot_reload.c
@@ -11,13 +11,34 @@
 #include <string.h>
 #include <sys/ioctl.h>
 #include <unistd.h>
+#include <fcntl.h>
 #include <dirent.h>
+#include <errno.h>
 #include <exanic/exanic.h>
 #include <exanic/ioctl.h>
 #include <exanic/register.h>
 #include <exanic/config.h>
 #include <exanic/util.h>
 #include "hot_reload.h"
+
+/*
+ * These definitions are defined by (or derived from) the PCIe 3.0
+ * base spec, Nov. 10 2010, mostly from sections 7.8 and 7.10.
+ */
+#define PCIE_CFG_SPACE_MAX_LEN      4096
+#define PCIE_EXT_CAP_FIRST          0x100
+#define PCIE_EXT_CAP_ID_MASK        0x0000FFFF
+#define PCIE_EXT_CAP_PTR_MASK       0xFFF00000
+#define PCIE_EXT_CAP_PTR_SHIFT      20
+#define PCIE_AER_CAP_ID             0x0001
+#define PCIE_AER_SURPRISE_DOWN_MASK (1 << 5)
+#define PCIE_AER_EXTCAP_MASK_OFFSET 8
+typedef union {
+    uint8_t b[PCIE_CFG_SPACE_MAX_LEN];
+    uint16_t w[PCIE_CFG_SPACE_MAX_LEN/2];
+    uint32_t l[PCIE_CFG_SPACE_MAX_LEN/4];
+} pcie_cfg_space;
+typedef uint16_t pcie_cfg_space_offset;
 
 /*
  * Check that the currently loaded firmware has support for hot reloading
@@ -101,6 +122,46 @@ static bool write_1_to_file(char *filename)
 }
 
 /*
+ * Utility function to lseek and write
+ */
+static ssize_t write_at_offset(int fd, off_t offset, const void *buf, size_t count)
+{
+    if (lseek(fd, offset, SEEK_SET) == (off_t)-1)
+        return -1;
+
+    return write(fd, buf, count);
+}
+
+/*
+ * Given a PCIe config space of size `sz`, locate a PCIe Advanced Error
+ * Reporting (AER) structure and return a space-local pointer to its error
+ * mask. Can fail if the device does not support AER, or the size of the
+ * space indicates it doesn't have any extended capabilities.
+ */
+pcie_cfg_space_offset find_aer_mask(pcie_cfg_space *cfg_space, size_t sz)
+{
+    pcie_cfg_space_offset cap_ptr = PCIE_EXT_CAP_FIRST;
+    uint32_t cap_id;
+
+    if (sz < PCIE_EXT_CAP_FIRST)
+        return 0;
+
+    while (cap_ptr != 0
+           && (cap_ptr + PCIE_AER_EXTCAP_MASK_OFFSET + sizeof(uint32_t)) < sz)
+    {
+        cap_id = cfg_space->l[cap_ptr/4] & PCIE_EXT_CAP_ID_MASK;
+
+        if (cap_id == PCIE_AER_CAP_ID)
+            return cap_ptr + PCIE_AER_EXTCAP_MASK_OFFSET;
+
+        cap_ptr = (cfg_space->l[cap_ptr/4]
+                   & PCIE_EXT_CAP_PTR_MASK) >> PCIE_EXT_CAP_PTR_SHIFT;
+    }
+
+    return 0;
+}
+
+/*
  * Reload firmware without requiring a host reboot
  */
 exanic_t *reload_firmware(exanic_t *exanic, void (*report_progress)())
@@ -109,11 +170,17 @@ exanic_t *reload_firmware(exanic_t *exanic, void (*report_progress)())
     char remove_path[256];
     char device_path[256];
     char resolved_path[PATH_MAX];
-    DIR *d;
-    struct dirent *dir;
+    char parent_config_path[PATH_MAX];
+    int parent_config_fd;
+    pcie_cfg_space parent_config_space;
+    ssize_t parent_config_space_size;
+    DIR *dir;
+    struct dirent *dirent;
     char new_device_name[64];
     int  new_device_port;
     unsigned int attempts;
+    pcie_cfg_space_offset aer_mask_offset = 0;
+    uint32_t aer_mask, old_aer_mask;
 
     /* Get the interface name of the first port on the device */
     if (exanic_get_interface_name(exanic, 0, ifname, sizeof(ifname)) == -1)
@@ -132,6 +199,25 @@ exanic_t *reload_firmware(exanic_t *exanic, void (*report_progress)())
         fprintf(stderr, "ERROR: unable to determine real path of %s\n", device_path);
         exanic_release_handle(exanic);
         return NULL;
+    }
+
+    /* Attempt to mask surprise down event on parent bridge */
+    snprintf(parent_config_path, sizeof(parent_config_path), "/sys/class/net/%s/device/../config", ifname);
+    parent_config_fd = open(parent_config_path, O_RDWR);
+    if (parent_config_fd != -1)
+    {
+        parent_config_space_size = read(parent_config_fd, parent_config_space.b, PCIE_CFG_SPACE_MAX_LEN);
+        if (parent_config_space_size > 0)
+        {
+            aer_mask_offset = find_aer_mask(&parent_config_space, parent_config_space_size);
+            if (aer_mask_offset != 0)
+            {
+                aer_mask = old_aer_mask = parent_config_space.l[aer_mask_offset/4];
+                aer_mask |= htole32(PCIE_AER_SURPRISE_DOWN_MASK);
+                if (write_at_offset(parent_config_fd, aer_mask_offset, &aer_mask, sizeof(uint32_t)) != sizeof(uint32_t))
+                    fprintf(stderr, "WARNING: attempt to ignore \"surprise down\" event failed (%s)\n", strerror(errno));
+            }
+        }
     }
 
     snprintf(remove_path, sizeof(remove_path), "/sys/class/net/%s/device/remove", ifname);
@@ -155,33 +241,42 @@ exanic_t *reload_firmware(exanic_t *exanic, void (*report_progress)())
         report_progress();
 
         /* Open the sysfs path corresponding to the PCI device we are using that we saved before */
-        d = opendir(resolved_path);
-        if (d != NULL)
+        dir = opendir(resolved_path);
+        if (dir != NULL)
             break;
     }
 
-    if (d == NULL)
+    /* Restore the previous AER mask in the parent bridge */
+    if (parent_config_fd != -1)
+    {
+        if (aer_mask_offset != 0)
+            write_at_offset(parent_config_fd, aer_mask_offset, &old_aer_mask, sizeof(uint32_t));
+        close(parent_config_fd);
+    }
+
+    if (dir == NULL)
     {
         fprintf(stderr, "ERROR: device did not reappear at %s after hot reload. If you cannot find the card in lspci, a host reboot or recovery mode boot may be required.\n", resolved_path);
         return NULL;
     }
 
     /* Find a file in this directory that looks like a network interface */
-    while ((dir = readdir(d)) != NULL)
+    while ((dirent = readdir(dir)) != NULL)
     {
-        if (strcmp(dir->d_name, ".") && strcmp(dir->d_name, "..") && dir->d_type == DT_DIR)
+        if (strcmp(dirent->d_name, ".") && strcmp(dirent->d_name, "..") && dirent->d_type == DT_DIR)
             break;
     }
+    closedir(dir);
 
-    if (dir == NULL)
+    if (dirent == NULL)
     {
         fprintf(stderr, "ERROR: unable to find network interface in directory: %s\n", resolved_path);
         return NULL;
     }
 
-    if (exanic_find_port_by_interface_name(dir->d_name, new_device_name, sizeof(new_device_name), &new_device_port) == -1)
+    if (exanic_find_port_by_interface_name(dirent->d_name, new_device_name, sizeof(new_device_name), &new_device_port) == -1)
     {
-        fprintf(stderr, "ERROR: unable to get exanic device name for interface name: %s\n", dir->d_name);
+        fprintf(stderr, "ERROR: unable to get exanic device name for interface name: %s\n", dirent->d_name);
         return NULL;
     }
 
