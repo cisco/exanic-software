cf0c6fb95f697644885701fba45221d6eaac37cd
Sandeep Malik <sandipml@cisco.com>
2025-05-31 08:04:03 +1000

    Release 2.7.5: Update the version and documentation for new release.

--- include/exanic_version.h
+++ include/exanic_version.h
@@ -5,7 +5,7 @@
 
 #define EXANIC_VERSION_MAJOR        2
 #define EXANIC_VERSION_MINOR        7
-#define EXANIC_VERSION_REV          4
+#define EXANIC_VERSION_REV          5
 #define EXANIC_VERSION_EXTRA        "-git"
 
 /* Do not edit these macros */
